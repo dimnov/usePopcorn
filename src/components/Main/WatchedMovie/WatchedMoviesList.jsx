47a70cad14a39f4b57e6704ad685a61c6938a4eb
dimnov <dimnov1512@gmail.com>
2024-05-01 15:59:02 +0300

    Add deleting button for watched movies

--- src/components/Main/WatchedMovie/WatchedMoviesList.jsx
+++ src/components/Main/WatchedMovie/WatchedMoviesList.jsx
@@ -1,13 +1,13 @@
 import WatchedMovie from "./WatchedMovie.jsx";
 
-function WatchedMoviesList({ watched }) {
- return (
-  <ul className="list">
-   {watched.map((movie) => (
-    <WatchedMovie movie={movie} key={movie.imdbId} />
-   ))}
-  </ul>
- );
+function WatchedMoviesList({ watched, onDeleteWatched }) {
+  return (
+    <ul className="list">
+      {watched.map((movie) => (
+        <WatchedMovie movie={movie} key={movie.imdbID} onDeleteWatched={onDeleteWatched} />
+      ))}
+    </ul>
+  );
 }
 
 export default WatchedMoviesList;
