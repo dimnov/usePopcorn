c6b8e8231b2dcb556fcf2a7503660ccdf040b604
dimnov <dimnov1512@gmail.com>
2024-05-04 00:34:15 +0300

    Add custom hook for movie fetch

---
+++ src/hooks/useMovies.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { KEY } from "../config.js";
+
+export function useMovies(query) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    async function fetchMovies() {
+      try {
+        setIsLoading(true);
+        setError("");
+
+        const res = await fetch(`http://www.omdbapi.com/?apiKey=${KEY}&s=${query}`, {
+          signal: controller.signal,
+        });
+
+        if (!res.ok) throw new Error("No internet connection");
+
+        const data = await res.json();
+        if (data.Response === "False") throw new Error("Movie not found");
+
+        setMovies(data.Search);
+        setError("");
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          setError(error.message);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    if (query.length < 3) {
+      setMovies([]);
+      setError("");
+      return;
+    }
+
+    fetchMovies();
+
+    return () => {
+      controller.abort();
+    };
+  }, [query]);
+
+  return { movies, isLoading, error };
+}
