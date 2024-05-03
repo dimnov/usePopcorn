663ef84997cec77c2f039dc42c98da9306b8e5c8
dimnov <dimnov1512@gmail.com>
2024-05-03 21:24:03 +0300

    Focus on search when "enter" btn is clicked

--- src/components/Navigation/Search.jsx
+++ src/components/Navigation/Search.jsx
@@ -1,4 +1,26 @@
+import { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+
 function Search({ query, handleSearch }) {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    function callback(e) {
+      if (document.activeElement === inputEl.current) {
+        return;
+      }
+
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        handleSearch("");
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+
+    return () => document.addEventListener("keydown", callback);
+  }, [handleSearch]);
+
   return (
     <input
       className="search"
@@ -6,8 +28,14 @@ function Search({ query, handleSearch }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => handleSearch(e.target.value)}
+      ref={inputEl}
     />
   );
 }
+
+Search.propTypes = {
+  query: PropTypes.string,
+  handleSearch: PropTypes.func,
+};
 
 export default Search;
